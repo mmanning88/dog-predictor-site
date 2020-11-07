8e0ecc5da6662e99dee5a483519c649aeb88c08c
Matthew Manning <mmanning88@gmail.com>
2020-11-06 22:14:29 -0700

    add allowed host

--- dogpredictor/settings.py
+++ dogpredictor/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', "00000")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['lit-meadow-30837.herokuapp.com']
 
 # Application definition
 
