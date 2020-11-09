ab73060454da795e18843afce380a4c1a877e8e2
Matthew Manning <mmanning88@gmail.com>
2020-11-09 12:01:22 -0700

    Change whitenoise setting

--- dogpredictor/settings.py
+++ dogpredictor/settings.py
@@ -62,7 +62,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 ROOT_URLCONF = 'dogpredictor.urls'
 
